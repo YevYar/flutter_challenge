01f7aa962367572934074d4481293eb29a1c79f7
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-09-21 17:25:04 +0300

    Import update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_challenge/routes/router.gr.dart';
+
+import 'routes/router.gr.dart';
 
 void main() {
   runApp(MyApp());
