8c0b9ff17e258866c81a99e60d08c9603e20129c
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-10-05 16:17:45 +0300

    fetchMovie response handling was implemented

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -53,7 +53,7 @@ class Api {
 
   /// Returns movie by [id]
   ///
-  /// Can return FailedResponse
+  /// Can return MovieDetailing or FailedResponse
   Future<dynamic> fetchMovie(String id) async {
     Response response = await _httpService.get('${ApiUrl["fetchMovie"]}$id&plot=full', params: params);
     return fetchMovieUtil(response);
--- lib/utils/api_utils.dart
+++ lib/utils/api_utils.dart
@@ -1,17 +1,34 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_challenge/api/models/models.dart';
+import 'package:flutter_challenge/redux/movies/models/models.dart';
 
 searchUtil(Response response) {
   if (response.data is SuccessResponse) {
     print('data: ${(response.data as SuccessResponse).data}');
 
     return SearchOkResponse(
-      (response.data.data["Search"] as List<dynamic>).cast<Map<String, String>>(),
-      int.parse(response.data.data["totalResults"]));
+        (response.data.data["Search"] as List<dynamic>).cast<Map<String, String>>(),
+        int.parse(response.data.data["totalResults"]));
   } else
     return (response.data as FailedResponse);
 }
 
 fetchMovieUtil(Response response) {
+  if (response.data is SuccessResponse) {
+    print('data: ${(response.data as SuccessResponse).data}');
 
+    Map<String, dynamic> data = response.data.data;
+
+    return MovieDetailing(
+        genre: data["Genre"],
+        director: data["Director"],
+        fullPlot: data["Plot"],
+        cast: data["Actors"],
+        ratings: (data["Ratings"] as List).map((item) => Rating(
+              item["Source"],
+              item["Value"],
+            )).toList()
+        );
+  } else
+    return (response.data as FailedResponse);
 }
