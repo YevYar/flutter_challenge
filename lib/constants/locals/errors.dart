61bf008bf2a5cafd72b4ccbdaf4ab4a210c95c8f
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-10-11 23:13:33 +0300

    Constants were updated

---
+++ lib/constants/locals/errors.dart
@@ -0,0 +1,9 @@
+const Map<String, String> errors = {
+  "emptyTitle": "Please, provide movie title to search",
+  "infoDescription": "We cannot fetch detailed information about this movie. Error: \n",
+  "infoTitle": "Something has gone wrong!",
+
+  "clientError": "Bad request",
+  "networkError": "Please check internet connection",
+  "serverError": "Server error",
+};
