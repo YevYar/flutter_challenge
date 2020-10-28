4756adec36b858a048cd50d5d368b5bcffccc9fa
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-10-29 00:14:50 +0200

    Testing connecting to Store

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,38 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
+import 'redux/movies/models/models.dart';
+import 'redux/redux.dart';
 import 'routes/router.gr.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(store: configureStore()));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final Store<MovieState> store;
+
+  MyApp({Key key, this.store}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Challenge',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      builder: ExtendedNavigator.builder<Router>(
-        router: Router(),
-        initialRoute: Routes.searchScreen,
-      ),
-    );
+    return StoreProvider<MovieState>(
+        // Pass the store to the StoreProvider. Any ancestor `StoreConnector`
+        // Widgets will find and use this value as the `Store`.
+        store: store,
+        child: MaterialApp(
+          title: 'Flutter Challenge',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          builder: ExtendedNavigator.builder<Router>(
+            router: Router(),
+            initialRoute: Routes.searchScreen,
+          ),
+        ));
   }
 }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_challenge/api/api.dart';
+import 'package:flutter_challenge/redux/movies/models/models.dart';
+import 'package:flutter_challenge/redux/movies/movies.dart';
 import 'package:flutter_challenge/routes/router.gr.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class SearchScreen extends StatelessWidget {
   @override
@@ -9,10 +13,30 @@ class SearchScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Home"),
       ),
-      body: RaisedButton(
+      body: StoreConnector<MovieState, VoidCallback>(
+        converter: (store) {
+          // Return a `VoidCallback`, which is a fancy name for a function
+          // with no parameters. It only dispatches an Increment action.
+          return () => store.dispatch(FindMoviesAction("fgdgghfdhfh"));
+        },
+        builder: (context, callback) {
+          return RaisedButton(child: const Text("Search"), onPressed: callback);
+        },
+      ),
+      floatingActionButton: StoreConnector<MovieState, VoidCallback>(
+        converter: (store) {
+          // Return a `VoidCallback`, which is a fancy name for a function
+          // with no parameters. It only dispatches an Increment action.
+          return () => store.dispatch(FetchMovieAction(""));
+        },
+        builder: (context, callback) {
+          return FloatingActionButton(onPressed: callback);
+        },
+      ),
+      /*RaisedButton(
         child: const Text("Go to Movie"),
-        onPressed: () => ExtendedNavigator.root.pushMovieScreen(),
-      )
+        onPressed: () async => print(await Api.instance.fetchMovie("tt0480249")), // .then((value) => print(value)); // ExtendedNavigator.root.pushMovieScreen(),
+      )*/
     );
   }
 }
